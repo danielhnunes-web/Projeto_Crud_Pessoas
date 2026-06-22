af9c7391de5b2bbd3c5c7492645ac8b8494d9cd0
Daniel <daniel.h.nunes@aluno.senai.br>
2026-06-22 14:41:46 -0300

    Criado o arquivo scripts_pessoas.js. Pego os elementos do DOM; criado o array PESSOAS; criado a função formPessoa

---
+++ js/scripts_pessoas.js
@@ -0,0 +1,23 @@
+// Pegando Elementos Do DOM
+const formPessoa = document.querySelector('#form-pessoa')
+const divLista = document.querySelector('#div-lista-pessoas')
+
+// Criando ARRAY pessoas 
+const pessoas = []
+
+// CAPTURAR O EVENTO submit DO FORMULARIO
+formPessoa.addEventListener('submit', (evt) => {
+    //INTERROMPER O EFEITO PADRÃO DE SUBMETER AOS DADOS DO FORMULÁRIO
+    evt.preventDefault()
+
+    //CRIAR UM OBJETO FORMULÁRIO
+    const dadosFormPessoa = new FormData(formPessoa)
+
+    //CRIAR UM OBJETO LITERAL
+    const pessoa = {
+        nome: dadosFormPessoa.get('nome'),
+        idade: dadosFormPessoa.get('idade'),
+        renda: dadosFormPessoa.get('renda')
+    }
+    
+})
